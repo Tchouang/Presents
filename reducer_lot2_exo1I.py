42f8b43ae088d3e1055eab0467073952a817809a
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-03-05 12:50:40 +0100

    reducer exo1I V2

--- reducer_lot2_exo1I.py
+++ reducer_lot2_exo1I.py
@@ -9,14 +9,21 @@ import pandas as pd
 # Fonction pour traiter une ligne de données et mettre à jour le dictionnaire mydata
 def process_line(line, commandes):
     cpcli, datcde, codcde, timbrecde, Nbcolis, qte, points = line.strip().split(";")
+    if Nbcolis == "NULL": Nbcolis=0
+    else: Nbcolis=int(Nbcolis)  
+    if qte == "NULL": qte=0
+    else: qte=int(qte)         
+    if points =="NULL": points=0 
+    else:points=int(points)   
     if codcde not in commandes:
         commandes[codcde] = {
-            "cpcli": cpcli,
             "datcde": datcde,
+            "codcde" : codcde,
+            "cpcli": cpcli,
             "timbrecde": timbrecde,
             "Nbcolis": Nbcolis,
             "qte": qte,
-            "points": points
+            "points": points,
         }
     else:
         # Si le code de commande existe déjà, mettre à jour les informations d'agrégation
@@ -41,12 +48,12 @@ sorted_commande = sorted (
 #Création Dataframe
 
 mydata = []
-for codcde, value in sorted_commande[:100]:
+for codcde, value in sorted_commande:
     mydata.append(
         [
             codcde,
-            value["cpcli"],
             value["datcde"],
+            value["cpcli"],
             value["timbrecde"],
             value["Nbcolis"],
             value["qte"],
@@ -58,9 +65,9 @@ for codcde, value in sorted_commande[:100]:
 df = pd.DataFrame(
     mydata,
     columns=[
-        "cpcli",
         "datcde",
         "codcde",
+        "cpcli",
         "timbrecde",
         "Nbcolis",
         "qte",
