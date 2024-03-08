3311c98db6ca1b3512c35ff82dc015b96ded88d2
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-03-08 16:52:29 +0100

    update importscrupt question3folder

--- Question-3/import_script.py
+++ Question-3/import_script.py
@@ -10,7 +10,13 @@ logging.basicConfig(level=logging.INFO)
 
 connection = happybase.Connection('node176250-env-1839015-etudiant37.sh1.hidora.com', 11775) #VM 37 à changer par user
 connection.open()
-table_name = 'fromagerie'  #à changer si besoin
+
+connection.create_table(
+            "fromagerie_pres",
+            {
+                'cf': dict(),
+            })
+table_name = 'fromagerie_pres'  #à changer si besoin
 table = connection.table(table_name)
 
 # Fonction pour vérifier si une chaîne peut être convertie en date valide
@@ -24,11 +30,9 @@ def is_valid_datcde(datcde_str):
 # Fonction pour importer les données de datawfro dans HBase
 def import_csv_to_hbase(csv_file):
     try:
-        print('avant ouverture fichier')
         with open(csv_file, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             next(reader)
-            print('ouverture fichier')
             for row in reader:
                 # Générer une clé de ligne unique
                 row_key = str(uuid.uuid4())
@@ -36,16 +40,14 @@ def import_csv_to_hbase(csv_file):
                 if not is_valid_datcde(row['datcde']):
                     logging.warning("Date invalide dans la ligne: %s", row)
                     continue
-                print('print du row')
                 # Remplacer les valeurs NULL par 0 pour les autres données hors date
                 for key, value in row.items():
                     if key != 'datcde' and (value.strip() == '' or value.strip().lower() == 'null'):
                         row[key] = '0' if key.isdigit() else '0'
-                print('avant le put')
                 table.put(str(row_key).encode(), {
                     b'cf:codcli': str(row['codcli']).encode(),
                     b'cf:genrecli': str(row['genrecli']).encode(),
-                    b'cd:nomcli': str(row['nomcli']).encode(),
+                    b'cf:nomcli': str(row['nomcli']).encode(),
                     b'cf:prenomcli': str(row['prenomcli']).encode(),
                     b'cf:cpcli': str(row['cpcli']).encode(),
                     b'cf:villecli': str(row['villecli']).encode(),
@@ -78,7 +80,7 @@ if connection is not None:
 else:
     logging.error("Erreur: La connexion à HBase n'est pas établie")
 
-import_csv_to_hbase('dataw_fro03_mini_100.csv')
+import_csv_to_hbase('dataw_fro03.csv')
 
 # Après l'importation des données
 if connection is not None:
