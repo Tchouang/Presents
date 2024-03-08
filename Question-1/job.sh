366e24459ed05c4110d167856554bd31decf206d
Pierrick-LB <146710146+Pierrick-LB@users.noreply.github.com>
2024-03-08 16:57:14 +0100

    Delete Question-1 directory

--- Question-1/job.sh
+++
@@ -1,4 +0,0 @@
-./start-hadoop.sh
-hdfs dfs -rm -r ./output/output_lot2_exo1
-hadoop jar hadoop-streaming-2.7.2.jar -file mapper_lot2_exo1.py -mapper "python3 mapper_lot2_exo1.py" -file reducer_lot2_exo1.py -reducer "python3 reducer_lot2_exo1.py" -input input/dataw_fro03.csv -output output/output_lot2_exo1
-
